28403446b839aedead795d42f17e3c2e0d0f97d5
Matt Rosno <matt.rosno@gmail.com>
2025-01-15 19:05:03 -0600

    initial mobile nav

--- app/components/header.jsx
+++ app/components/header.jsx
@@ -1,6 +1,7 @@
 import { useEventListener } from "@/hooks";
 import { Link } from "@remix-run/react";
 import { NavLink, useMatches } from "@remix-run/react";
+import PropTypes from "prop-types";
 import { useState, useEffect, useCallback } from "react";
 
 import { Button } from "@/components/ui/button";
@@ -9,12 +10,31 @@ import { Sun } from "@/icons";
 
 import { cn } from "@/lib/utils";
 
+const MobileNavItem = ({ children, to }) => (
+  <li className="border-b border-ghp-250 bg-background">
+    <Link
+      className="block py-8 text-center font-serif text-4xl text-orange decoration-2
+        underline-offset-4 outline-none hover:underline focus-visible:underline"
+      prefetch="viewport"
+      to={to}
+    >
+      {children}
+    </Link>
+  </li>
+);
+
+MobileNavItem.propTypes = {
+  children: PropTypes.node.isRequired,
+  to: PropTypes.string.isRequired,
+};
+
 const Header = () => {
   const matches = useMatches();
   const [isScrolled, setIsScrolled] = useState(false);
+  const [menuOpen, setMenuOpen] = useState(false);
 
   const isHome = matches[1]?.pathname === "/";
-  const invertColors = isHome && !isScrolled;
+  const invertColors = isHome && !isScrolled && !menuOpen;
 
   const scrollHandler = useCallback(() => {
     setIsScrolled(window.scrollY > 0);
@@ -26,90 +46,105 @@ const Header = () => {
   }, [scrollHandler]);
 
   return (
-    <div
-      className={cn(
-        "relative flex h-24 items-center justify-between px-5 lg:px-16",
-        !invertColors && "bg-background",
-        invertColors ? "text-white" : "border-b border-ghp-250 text-orange",
-      )}
-    >
-      <ul className="-mx-2.5">
-        <li className="inline-block px-2.5 lg:hidden">
-          <Link
-            className="inline-block font-serif outline-none hover:underline focus-visible:underline"
-            prefetch="viewport"
-            to="/"
-          >
-            Golden Hour
-          </Link>
-        </li>
-        <li className="hidden px-2.5 lg:inline-block">
-          <NavLink
-            className={({ isActive }) =>
-              cn(
-                "inline-block outline-none hover:underline focus-visible:underline",
-                isActive && "underline",
-              )
-            }
-            prefetch="viewport"
-            to="/pools"
-          >
-            Our Pools
-          </NavLink>
-        </li>
-        <li className="hidden px-2.5 lg:inline-block">
-          <NavLink
-            className={({ isActive }) =>
-              cn(
-                "inline-block outline-none hover:underline focus-visible:underline",
-                isActive && "underline",
-              )
-            }
-            prefetch="viewport"
-            to="/gallery"
-          >
-            Gallery
-          </NavLink>
-        </li>
-      </ul>
-      <Link
-        className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 rounded-full
-          border-current outline-none focus-visible:border"
-        prefetch="viewport"
-        to="/"
+    <>
+      <div
+        className={cn(
+          "relative flex h-24 items-center justify-between px-5 lg:px-16",
+          !invertColors && "bg-background",
+          invertColors ? "text-white" : "border-b border-ghp-250 text-orange",
+        )}
       >
-        <Sun className="animate h-12 w-12 animate-spin [animation-duration:_20s]" />
-      </Link>
-      <ul className="-mx-2.5">
-        <li className="hidden px-2.5 lg:inline-block">
-          <NavLink
-            className={({ isActive }) =>
-              cn(
-                "inline-block outline-none hover:underline focus-visible:underline",
-                isActive && "underline",
-              )
-            }
-            prefetch="viewport"
-            to="/faq"
-          >
-            FAQ
-          </NavLink>
-        </li>
-        <li className="hidden px-2.5 lg:inline-block">
-          <Button asChild kind="outline" size="sm">
-            <Link to="/contact">Contact</Link>
-          </Button>
-        </li>
-        <li className="inline-block px-2.5 lg:hidden">
-          <button
-            className="inline-block cursor-pointer outline-none hover:underline
-              focus-visible:underline"
-          >
-            Menu
-          </button>
-        </li>
-      </ul>
-    </div>
+        <ul className="-mx-2.5">
+          <li className="inline-block px-2.5 lg:hidden">
+            <Link
+              className="inline-block font-serif underline-offset-2 outline-none hover:underline
+                focus-visible:underline"
+              prefetch="viewport"
+              to="/"
+            >
+              Golden Hour
+            </Link>
+          </li>
+          <li className="hidden px-2.5 lg:inline-block">
+            <NavLink
+              className={({ isActive }) =>
+                cn(
+                  `inline-block underline-offset-2 outline-none hover:underline
+                  focus-visible:underline`,
+                  isActive && "underline",
+                )
+              }
+              prefetch="viewport"
+              to="/pools"
+            >
+              Our Pools
+            </NavLink>
+          </li>
+          <li className="hidden px-2.5 lg:inline-block">
+            <NavLink
+              className={({ isActive }) =>
+                cn(
+                  `inline-block underline-offset-2 outline-none hover:underline
+                  focus-visible:underline`,
+                  isActive && "underline",
+                )
+              }
+              prefetch="viewport"
+              to="/gallery"
+            >
+              Gallery
+            </NavLink>
+          </li>
+        </ul>
+        <Link
+          className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 rounded-full
+            border-current outline-none focus-visible:border"
+          prefetch="viewport"
+          to="/"
+        >
+          <Sun className="animate h-12 w-12 animate-spin [animation-duration:_20s]" />
+        </Link>
+        <ul className="-mx-2.5">
+          <li className="hidden px-2.5 lg:inline-block">
+            <NavLink
+              className={({ isActive }) =>
+                cn(
+                  `inline-block underline-offset-2 outline-none hover:underline
+                  focus-visible:underline`,
+                  isActive && "underline",
+                )
+              }
+              prefetch="viewport"
+              to="/faq"
+            >
+              FAQ
+            </NavLink>
+          </li>
+          <li className="hidden px-2.5 lg:inline-block">
+            <Button asChild kind="outline" size="sm">
+              <Link to="/contact">Contact</Link>
+            </Button>
+          </li>
+          <li className="inline-block px-2.5 lg:hidden">
+            <button
+              className="inline-block cursor-pointer underline-offset-2 outline-none hover:underline
+                focus-visible:underline"
+              onClick={() => setMenuOpen(!menuOpen)}
+            >
+              {menuOpen ? "Close" : "Menu"}
+            </button>
+          </li>
+        </ul>
+      </div>
+      {menuOpen && (
+        <ul className="absolute h-dvh w-full bg-ghp-200 lg:hidden">
+          <MobileNavItem to="/pools">Our Pools</MobileNavItem>
+          <MobileNavItem to="/gallery">Gallery</MobileNavItem>
+          <MobileNavItem to="/faq">FAQ</MobileNavItem>
+          <MobileNavItem to="/contact">Contact</MobileNavItem>
+        </ul>
+      )}
+    </>
   );
 };
 
--- app/components/hero.jsx
+++ app/components/hero.jsx
@@ -10,7 +10,9 @@ const Hero = () => {
         className="absolute top-0 left-0 h-72 w-full bg-gradient-to-b from-ghp-900 to-transparent"
       ></div>
       <div className="absolute top-0 left-0 flex h-full w-full flex-col items-center px-5">
-        <h1 className="shrink-0 pt-36 font-serif leading-0 text-white">Golden Hour</h1>
+        <h1 className="hidden shrink-0 pt-36 font-serif leading-0 text-white lg:inline">
+          Golden Hour
+        </h1>
         <div className="flex grow flex-col items-center justify-center gap-6 lg:gap-8">
           <h2 className="text-center font-serif text-4xl text-white sm:text-5xl">
             Great Pools, <br className="lg:hidden" />
