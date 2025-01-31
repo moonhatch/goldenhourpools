fc4652607563c9b9e7b364c14a2354d324db0890
Matt Rosno <matt.rosno@gmail.com>
2025-01-31 17:59:17 -0600

    add checkbox, card addons in form

--- app/lib/utils.ts
+++ app/lib/utils.ts
@@ -4,3 +4,12 @@ import { twMerge } from "tailwind-merge";
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+export function formatCurrency(number = 0) {
+  return new Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: "USD",
+    maximumFractionDigits: 0,
+    minimumFractionDigits: 0,
+  }).format(number ?? 0);
+}
