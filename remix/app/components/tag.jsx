423d9228fc04eb9ac08739bce9219f628ab19481
Matt Rosno <matt.rosno@gmail.com>
2025-07-27 18:52:03 -0500

    feat: style media and tag

--- remix/app/components/tag.jsx
+++ remix/app/components/tag.jsx
@@ -1,7 +1,7 @@
 import PropTypes from "prop-types";
 
 const Tag = ({ children }) => {
-  return <div className="rounded-md bg-white px-3 py-0.5 text-black">{children}</div>;
+  return <div className="font-serif text-xl leading-none text-white uppercase">{children}</div>;
 };
 
 Tag.propTypes = {
